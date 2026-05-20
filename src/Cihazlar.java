5a589c998855443ff565c9bb8ca402c371581c93
Mert Ömer Ezer <mertomer346@gmail.com>
2026-05-20 19:25:28 +0300

    Devices class completed.

---
+++ src/Cihazlar.java
@@ -0,0 +1,41 @@
+public abstract class Cihazlar {
+
+    private String cihazAdi;
+    private boolean acikMi;
+
+    Cihazlar(String cihazAdi){
+        this.cihazAdi=cihazAdi;
+        this.acikMi=false;
+
+    }
+    public void ac(){
+    if(acikMi == false){
+        acikMi= true;
+        System.out.println(cihazAdi+" Açıldı");
+    }else{
+        System.out.println("Zaten açık");
+        }
+    }
+
+    public void kapa(){
+        if(acikMi==true){
+            acikMi=false;
+            System.out.println(cihazAdi+" Kapandı");
+        }else{
+            System.out.println("Zaten Kapalı");
+        }
+
+    }
+
+    public boolean isacikMi(){
+        return acikMi;
+
+    }
+
+    public String getCihazAdi(){
+
+        return cihazAdi;
+    }
+
+    abstract void durumGoster();
+}
