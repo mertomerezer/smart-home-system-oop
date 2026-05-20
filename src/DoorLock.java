7a6607aff811c9681dd0320a809e9ce62b83110b
Mert Ömer Ezer <mertomer346@gmail.com>
2026-05-20 23:16:03 +0300

    All device classes completed.

--- src/DoorLock.java
+++ src/DoorLock.java
@@ -1,2 +1,34 @@
-public class DoorLock {
+public class DoorLock extends Devices{
+    private boolean kilitliMi;
+    public DoorLock(String cihazAdi){
+        super(cihazAdi);
+        this.kilitliMi=false;
+    }
+
+    public void kilitle(){
+        if(kilitliMi==false){
+            kilitliMi=true;
+            System.out.println("Kapı Kilitlendi");
+        }else System.out.println("Kapı zaten kilitli");
+    }
+
+    public void kilidiAc(){
+        if(kilitliMi==true){
+            kilitliMi=false;
+            System.out.println("Kapı Kiliti Açıldı");
+        }else System.out.println("Kapının Kilidi Zaten Açık");
+    }
+
+    @Override
+    public void durumGoster() {
+        System.out.println(getCihazAdi());
+        if (isacikMi()) {
+            System.out.println("Kapı açık");
+        } else {
+            System.out.println("Kapı kapalı");
+        }
+
+        System.out.println("Kilit durumu: " + kilitliMi);
+    }
+
 }
